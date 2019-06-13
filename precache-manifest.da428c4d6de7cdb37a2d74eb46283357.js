35788334390ec62ce30d57db36e5de82bd93f070
Andrew Ivanov <ivanov-andrey15@mail.ru>
2019-06-14 00:23:49 +0300

    Updates

---
+++ precache-manifest.da428c4d6de7cdb37a2d74eb46283357.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713949a69a250ed0b70b0060973c7948",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "87f04fead200ea3816ed",
+    "url": "/react-shop/static/css/main.93f09c2e.chunk.css"
+  },
+  {
+    "revision": "d30fb258614a275e10df",
+    "url": "/react-shop/static/js/2.e88262f5.chunk.js"
+  },
+  {
+    "revision": "87f04fead200ea3816ed",
+    "url": "/react-shop/static/js/main.e4c8910b.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  },
+  {
+    "revision": "6e1b1b6f9b44dd438241e443edfb759f",
+    "url": "/react-shop/static/media/Courier New.6e1b1b6f.ttf"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/react-shop/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "cdc3f924911d207181faac2a5ff3df44",
+    "url": "/react-shop/static/media/product1.cdc3f924.jpg"
+  },
+  {
+    "revision": "8133ec824e94936108259d857ed7bcb1",
+    "url": "/react-shop/static/media/product2.8133ec82.jpg"
+  },
+  {
+    "revision": "a02766f952213e7c5ce5e427298d2304",
+    "url": "/react-shop/static/media/product3.a02766f9.jpg"
+  },
+  {
+    "revision": "918b8e641812b2f7f20e8d03473b4d2b",
+    "url": "/react-shop/static/media/product4.918b8e64.jpg"
+  }
+]);
